614425876b5415e45fc45e4a1e25de6fd328abd5
Hannah Salmon <hr.salmon@gmail.com>
2014-06-26 09:37:19 +1200

    Added first model test

--- hacker-news/spec/controllers/comments_controller_spec.rb
+++ hacker-news/spec/controllers/comments_controller_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe CommentsController do
 
+  let(:user) { double :user, id: 1 }
+  let(:post_1) { double :post, id: 1 }
+
   describe 'GET index' do
 
     it 'renders the index template' do
@@ -12,7 +15,6 @@ describe CommentsController do
 
   describe 'GET user_comments' do
 
-    let(:user) { double :user, id: 1 }
     let(:comment) { double :comment, user_id: user.id }
 
     it 'renders the index template' do
@@ -24,8 +26,6 @@ describe CommentsController do
 
   describe 'GET new' do
 
-    let(:post) { double :post, post_id: 1 }
-
     it 'renders the new template' do
       Post.stub(:find).and_return(post)
       get :new
@@ -33,18 +33,19 @@ describe CommentsController do
     end
   end
 
-  describe 'POST create' do
+  # describe 'POST create' do
 
-    let(:post) { double :post, post_id: 1 }
+  #   let(:comment) { double :comment, save: true }
 
-    context 'with successful save' do
+  #   context 'with successful save' do
 
-      # it ''
-      #   Post.stub(:find).and_return(post)
-      # end
-    end
-
-    context 'with unsuccessful save' do
-    end
-  end
+  #     it 'provides the correct flash message' do
+  #       Post.stub(:find).and_return(post_1)
+  #       post_1.stub(:comments, :build).and_return(comment)
+  #       comment.stub(:user_id=).with(user.id)
+  #       post :create, id: post_1.id
+  #       flash[:notice].should =~ /Successful comment creation!/
+  #     end
+  #   end
+  # end
 end
